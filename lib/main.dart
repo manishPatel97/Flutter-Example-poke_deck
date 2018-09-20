cec9a67013c133ef7095ed08bbc8bbbaaf664322
Manish patel <manish13058@iiitd.ac.in>
2018-09-20 19:19:53 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:poke_deck/Pokemon.dart';
+import 'package:poke_deck/PokemonDetail.dart';
+
+
+void main() => runApp(MaterialApp(
+  title: "Poke Deck",
+  home: HomePage(),
+  debugShowCheckedModeBanner: false,
+));
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState  createState(){
+    return new HomePageState();
+  }
+}
+class HomePageState extends State<HomePage> {
+
+var url = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+Pokehub pokehub;
+
+
+@override
+  void initState() {
+  super.initState();
+  fetchData();
+  }
+
+
+  fetchData() async{
+   var res = await http.get(url);
+   //print(res);
+    var decodedJson = jsonDecode(res.body);
+    pokehub = Pokehub.fromJson(decodedJson);
+   setState(() {});
+  }
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text("Poke App"),
+      backgroundColor: Colors.cyan,
+    ),
+    body: pokehub == null
+        ? Center(
+      child: CircularProgressIndicator(),
+    )
+        : GridView.count(
+      crossAxisCount: 2,
+      children: pokehub.pokemon
+          .map((poke) => Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: InkWell(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => PokeDetail(
+                      pokemon: poke,
+                    )));
+          },
+          child: Hero(
+            tag: poke.img,
+            child: Card(
+              child: Column(
+                mainAxisAlignment:
+                MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(poke.img))),
+                  ),
+                  Text(
+                    poke.name,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ))
+          .toList(),
+    ),
+    drawer: Drawer(),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      backgroundColor: Colors.cyan,
+      child: Icon(Icons.refresh),
+    ),
+  );
+}
+
+}
+
+
+
